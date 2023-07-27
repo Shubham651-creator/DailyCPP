c2d5466ebd0630822f785ebb4ce58f88f3ad00b5
ShubhamAwale <shubhamawale651@gmail.com>
2023-07-27 20:00:09 +0530

    implement helper member functino

---
+++ CPlusPlus/25July23/mobile.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Mobile
+{
+    // private functions are called as helper function
+
+    bool check()
+    {
+    }
+
+public:
+    void call()
+    {
+        check();
+    }
+
+    void message()
+    {
+        check();
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
