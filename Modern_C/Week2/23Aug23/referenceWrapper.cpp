32b8737f4740605d00288dd2c31c12b18b71b48c
ShubhamAwale <shubhamawale651@gmail.com>
2023-08-24 14:19:12 +0530

    intro of reference wrapper

---
+++ Modern_C/Week2/23Aug23/referenceWrapper.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+
+int main()
+{
+    int n1 = 10;
+
+    std::reference_wrapper<int> r1 = n1; // int& r1 = n1;
+
+    std::reference_wrapper<int> arr[1]{r1};
+
+    std::vector<std::reference_wrapper<int>> data{r1};
+
+    std::reference_wrapper<std::reference_wrapper<int>> ref_ref{r1};
+}k
