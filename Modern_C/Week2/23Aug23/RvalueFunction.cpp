6c74a995b16c0ea212b2dd40f12f863b471ce04b
ShubhamAwale <shubhamawale651@gmail.com>
2023-08-24 14:09:34 +0530

    modify lambda: pass variables to the lambda function

--- Modern_C/Week2/23Aug23/RvalueFunction.cpp
+++ Modern_C/Week2/23Aug23/RvalueFunction.cpp
@@ -8,22 +8,26 @@ void operation(std::list<int> data, std::function<int(int)> fn)
     {
         std::cout << fn(i) << '\n';
     }
+    std::cout << "print second position of list : " << fn(2) << '\n';
 }
 
 /*
     In Lambda function :
-    [] - capture clause
+    [] : capture clause - it is used to access variable declared
+    or made local to the enclosing function
 */
 
 int main()
 {
+    int n1 = 12;
+
     operation(
         std::list<int>{1, 23, 4},
 
-        [](int number) //temporary function(rvalue)
+        [&n1](int number) // temporary function(rvalue)
         {
             std::cout << "Square():of " << number << "\t is ";
+            std::cout << "\nCapture clause : " << n1 << "\n";
             return number * number;
-        }
-    );
+        });
 }
