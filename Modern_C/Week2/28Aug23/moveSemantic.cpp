fb4c64a0cd2cfb3a4ecf8b64231f36821aee12df
ShubhamAwale <shubhamawale651@gmail.com>
2023-08-28 15:42:13 +0530

    we used push_back because it stores already gernerated objects.

--- Modern_C/Week2/28Aug23/moveSemantic.cpp
+++ Modern_C/Week2/28Aug23/moveSemantic.cpp
@@ -8,15 +8,15 @@ using Pointer = std::unique_ptr<Employee>;
 
 int main()
 {
-    std::vector<Pointer> data{
-        // here you can copy the unique pointe which not possible
-        // so we used Move semantic
-        std::move(std::make_unique<Employee>(101, 100000.0f)),
-        std::move(std::make_unique<Employee>(102, 200000.0f)),
-        std::move(std::make_unique<Employee>(103, 300000.0f)),
-        std::move(std::make_unique<Employee>(104, 400000.0f)),
-        std::move(std::make_unique<Employee>(105, 600000.0f)),
-    };
+    std::vector<Pointer> data;
+    auto e1 = std::make_unique<Employee>(102, 200000.0f);
+    data.push_back(std::make_unique<Employee>(102, 200000.0f));
+    data.push_back(std::make_unique<Employee>(103, 300000.0f));
+    data.push_back(std::make_unique<Employee>(104, 400000.0f));
+    data.push_back(std::make_unique<Employee>(105, 600000.0f));
+    data.push_back(std::make_unique<Employee>(101, 100000.0f));
+
+    data.push_back(std::move(e1));
 
     return 0;
 }
