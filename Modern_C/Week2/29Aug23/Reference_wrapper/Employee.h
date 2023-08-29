91464a22f121cf91343995d9e5725cd7594b7770
ShubhamAwale <shubhamawale651@gmail.com>
2023-08-29 19:04:16 +0530

    define reference wrapper class

---
+++ Modern_C/Week2/29Aug23/Reference_wrapper/Employee.h
@@ -0,0 +1,37 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <iostream>
+#include <memory>
+#include<functional>
+#include"Project.h"
+
+using RefType = std::reference_wrapper<Project> ;
+
+class Employee
+{
+private:
+    int _id;
+    float salary;
+    RefType _active_project;
+
+public:
+    Employee() = delete;
+    Employee(const Employee &) = delete;
+    Employee(const Employee &&) = delete;
+    Employee operator=(Employee &) = delete;
+    Employee operator=(Employee &&) = delete;
+    Employee(int id, float sal, RefType project);
+    ~Employee() = default;
+
+    float getSalary() const { return salary; }
+
+    friend std::ostream &operator<<(std::ostream &os, const Employee &rhs);
+
+};
+
+using Pointer = std::unique_ptr<Employee>;
+
+float CalculateTax(const Pointer &obj);
+
+#endif // EMPLOYEE_H
