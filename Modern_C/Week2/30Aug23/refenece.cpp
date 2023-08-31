d09d705d842ba0d5caea171e80ef0638309fb530
ShubhamAwale <shubhamawale651@gmail.com>
2023-08-31 09:50:11 +0530

    implement reference using call by value and call by std::ref()

--- Modern_C/Week2/30Aug23/refenece.cpp
+++ Modern_C/Week2/30Aug23/refenece.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<functional>
+#include <functional>
 
 /*
     primitive(int, float)
@@ -18,14 +18,44 @@ void magic(int &data)
     std::cout << "Data value is " << data << '\n';
 }
 
+void old_function_without_ref(int data)
+{
+    std::cout << "Data address = " << &data << '\n';
+    std::cout << "Data value = " << data << '\n';
+}
+
 int main()
 {
 
     int n1 = 10;
 
-    magic(n1);                       // valid
-    // magic(&n1);                      // invalid : &n1 is a pointer
-    // magic(10);                       // ivalid : 10 is rvalue, we need lvalue
-    // magic(std::unique_ptr<int>(n1)); // invalid : we are passing a rvalue(smart pointer)
-    magic(std::ref(n1));             // redundant use of ref
+    std::cout << "\nFirst time \n";
+
+    old_function_without_ref(n1); // call by value
+    std::cout << "n1 address is " << &n1 << '\n';
+    std::cout << "n1 value is " << n1 << '\n';
+
+    ////////////////////////////////////////////////
+
+    /*
+        std::ref says (whatever is being passed inside std::ref MUST BE
+        ACCEPTED AS A REFERENCE REFARDLESS OF WHAT THE CODE SAYS )
+
+        std::ref , it gernerally use when your code is build by call by value
+        but now you have to build by call by ref
+    */
+
+    std::cout << "\nSecond time \n";
+
+    old_function_without_ref(std::ref(n1)); // call by ref
+    std::cout << "n1 address = " << &n1 << '\n';
+    std::cout << "n1 value = " << n1 << '\n';
 }
+
+/*
+    magic(n1);                       // valid
+    magic(&n1);                      // invalid : &n1 is a pointer
+    magic(10);                       // ivalid : 10 is rvalue, we need lvalue
+    magic(std::unique_ptr<int>(n1)); // invalid : we are passing a rvalue(smart pointer)
+    magic(std::ref(n1));             // redundant use of ref
+*/
