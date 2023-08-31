7895d97f4d3455e5f50cf477d1226b319f5e1516
ShubhamAwale <shubhamawale651@gmail.com>
2023-08-31 12:07:06 +0530

    introduction of Race condtion Problem

---
+++ Modern_C/Week3/31Aug23/RaceConditonProblem.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <thread>
+
+/*
+    Bash command :
+
+    > g++ -pthread RaceConditionProblem.cpp -o app
+    > for((i=0;i<100;i++));do ./app; done
+    
+    After compiler this command, you get different ouput
+
+    To solve this Race Condition problem:
+    1. lock less
+    2. lock based syn mechnisms
+
+*/
+
+int amount = 1000;
+
+void withdraw()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+        amount -= 10;
+    }
+}
+
+void deposite()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+        amount += 10;
+    }
+}
+
+int main()
+{
+    std::thread t1(&withdraw);
+    std::thread t2(&deposite);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "final amount : " << amount << "\n";
+}
+
+/*
+
+    t2 starts 
+    
+    amount = 1000[RAM] -----> ALU [  1000-10 =990    ]
+
+    t1 starts
+
+    amount = 990 [RAM] ------> ALU [  1000+10 = 1010  ]
+            [1010]        <----------
+
+
+                        OR
+
+    t1 starts 
+    
+    amount = 1000[RAM] -----> ALU [   1000+10 =1010   ]
+
+    t2 starts
+
+    amount = 1010 [RAM] ------> ALU [  1000-10 = 990  ]
+            [990]        <----------
+
+*/
+
