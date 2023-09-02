f90537b88bbdbfd1988acd9d401052720978227f
ShubhamAwale <shubhamawale651@gmail.com>
2023-09-02 08:07:58 +0530

    Constant objects can't access the non-const function or method.

---
+++ Modern_C/Week3/31Aug23/ConstObject.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Employee
+{
+private:
+    const int data; // once initize can't ba change further
+
+public:
+    Employee(int val)
+        : data(val) {}
+    ~Employee() {}
+
+    int getData() const
+    {
+        return data;
+    }
+
+    void Display(){
+        std::cout<<data<<'\n';
+    }
+};
+
+int main()
+{
+    Employee e1(8);
+    e1.getData();
+    e1.Display();
+
+    // on const object we can call or invoke only const functions
+    Employee const e2(34);
+    e2.getData();
+    // e2.Display(); //error : Constant object can't access non-const functions
+}
