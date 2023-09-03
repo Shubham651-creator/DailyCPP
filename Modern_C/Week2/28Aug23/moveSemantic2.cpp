160ac19551916a9543aabaf6cdf8fa41cf57137d
ShubhamAwale <shubhamawale651@gmail.com>
2023-09-03 10:52:18 +0530

    Add operator<< overloading

--- Modern_C/Week2/28Aug23/moveSemantic2.cpp
+++ Modern_C/Week2/28Aug23/moveSemantic2.cpp
@@ -12,7 +12,16 @@ public:
     Employee &operator=(Employee &&) = default;
     Employee(int id, std::string name) : _id(id), _name(name) {}
     ~Employee() = default;
+
+    friend std::ostream &operator<<(std::ostream &os, const Employee &rhs);
 };
+
+inline std::ostream &operator<<(std::ostream &os, const Employee &rhs)
+{
+    os << "_id: " << rhs._id
+       << " _name: " << rhs._name;
+    return os;
+}
 
 int main()
 {
@@ -20,7 +29,12 @@ int main()
     Employee e1(Employee(101, "shubhan")); // move constructor called
     Employee e2(std::move(e1));            // set e1 to nullptr and data bind with e2
 
+    std::cout << e1 << "\n"; // set e1 nullptr manually
+
     Employee e3(Employee(102, "awale"));
     e1 = std::move(e3); // called move operator constructor and set e3 to nullptr
+
+    std::cout << e3 << "\n"; // set e3 nullptr manually
+
     return 0;
 }
