c21ceb8784728a078494bdceab758f0307c35dc5
ShubhamAwale <shubhamawale651@gmail.com>
2023-09-06 14:53:49 +0530

    structured binding usecase 1

--- Modern_C/Week3/6Sept23/structured_binding.cpp
+++ Modern_C/Week3/6Sept23/structured_binding.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <array>
 
-struct S
+/*
+    rvalue :
+    int{}
+    std::string {}
+*/
+
+using Container = std::array<int, 3>;
+
+Container magic()
 {
-    mutable int x : 2;
-    volatile float y;
-};
-
-S f() { return S{1, 2.3f}; }
+    Container v1{11, 22, 55};
+    return v1; // return by move(lvalue) since c++17
+}
 
 int main()
 {
+    Container ans = magic();
 
-    const auto [x, y] = f();
+    for (auto &value : ans)
+    {
+        std::cout << value << '\n';
+    }
 
-    std::cout << "x:\t" << x << "\ny:\t" << y << "\n";
+    // classic way to do initalize
+    int a = ans[0];
+    int b = ans[1];
+    int c = ans[2];
+
+    // sturctured binding to do initalize
+    auto [x, y, z] = magic();
+
+    std::cout << "x : " << x
+              << " y : " << y
+              << " z : " << z << '\n';
 }
