4faea0b782248de00231eb0d0f2af267096c6716
ShubhamAwale <shubhamawale651@gmail.com>
2023-09-07 18:27:35 +0530

    useCase 2: implement structured binding on unordered map

--- Modern_C/Week3/6Sept23/structured_binding.cpp
+++ Modern_C/Week3/6Sept23/structured_binding.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <array>
+#include <unordered_map>
 
 /*
     rvalue :
-    int{}
-    std::string {}
+        int{}
+        std::string {}
 */
 
 using Container = std::array<int, 3>;
@@ -13,6 +14,16 @@ Container magic()
 {
     Container v1{11, 22, 55};
     return v1; // return by move(lvalue) since c++17
+
+    /*
+
+        > return Container{11,2,34}; // NO copy/move
+
+        - Copy Elision for initialization form temporaries
+        (prvalue - purely rvalue) is required since c++17.
+
+        - Callable copy/move constructor no longer requried.
+    */
 }
 
 int main()
@@ -35,4 +46,25 @@ int main()
     std::cout << "x : " << x
               << " y : " << y
               << " z : " << z << '\n';
+
+    //-----------useCase 2---------------------------------------------
+
+    std::unordered_map<int, std::string> m1{
+        {1, "Shubham"}, // key,value
+        {2, "Awale"},
+        {3, "Rohan"}};
+
+    // classic way of looping
+    for (auto itr = m1.begin(); itr != m1.end(); ++itr)
+    {
+        std::cout << "The label is : " << itr->first << "\n"
+                  << "The value is : " << itr->second << '\n';
+    }
+
+    // c++17 version structured binding
+    for (auto [label, data] : m1)
+    {
+        std::cout << "The label is : " << label << "\t"
+                  << "The value is : " << data << '\n';
+    }
 }
