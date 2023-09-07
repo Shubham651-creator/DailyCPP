83e7bc2509f8f1c666027286f63bb111fb352f1f
ShubhamAwale <shubhamawale651@gmail.com>
2023-09-07 18:22:36 +0530

    Modify: implement by functional programming and reference wrapper

--- Problems/ModernCpp_marathon_1/TouristVehicle/Main.cpp
+++ Problems/ModernCpp_marathon_1/TouristVehicle/Main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <list>
+#include <vector>
 #include <memory>
 #include "TouristVehicle.h"
 #include "Permit.h"
 #include "Functionalites.h"
 
+using permitContainer = std::vector<std::shared_ptr<Permit>>;
 // list container which stores tourist objects
 using touristVehicleContainer = std::list<std::shared_ptr<TouristVehicle>>;
+using funPointer = std::function<touristVehicleContainer(touristVehicleContainer &, int)>;
 
 int main()
 {
     touristVehicleContainer touristObjects;
+    permitContainer permitObject;
 
     try
     {
-        CreateObjectsOfTourist(touristObjects);
+        CreateObjectsOfTourist(touristObjects, permitObject);
 
         // display the information
         for (std::shared_ptr<TouristVehicle> value : touristObjects)
@@ -24,23 +28,27 @@ int main()
 
         std::cout << "\n=============================================================\n";
 
-        std::cout<<"display the instance whose seat count >= 4 and permit type is lease\n";
-        for (std::shared_ptr<TouristVehicle> value : ConditionSeatCountAndPermitType(touristObjects))
+        funPointer fptr1 = &ConditionSeatCountAndPermitType;
+
+        std::cout << "display the instance whose seat count >= 4 and permit type is lease\n";
+        for (std::shared_ptr<TouristVehicle> value : Operation(touristObjects, fptr1))
         {
             std::cout << *value << "\n";
         }
         std::cout << "\n=============================================================\n";
 
-        std::cout<<"The average of booking charge of type CAB is "
-        <<AvergeOfTypeCAB(touristObjects)
-        <<".\n=========================================================\n";
+        std::cout << "The average of booking charge of type CAB is "
+                  << AvergeOfTypeCAB(touristObjects)
+                  << ".\n=========================================================\n";
 
-        std::cout<<"The maximum booking charge and its serial number is "
-        <<BookingChargeIsMaximum(touristObjects)
-        <<".\n==============================================================\n";
+        std::cout << "The maximum booking charge and its serial number is "
+                  << BookingChargeIsMaximum(touristObjects)
+                  << ".\n==============================================================\n";
 
-        std::cout<<"The first N instances are \n";
-        for (std::shared_ptr<TouristVehicle> value : FirstNInstance(touristObjects, 2))
+        std::cout << "The first N instances are \n";
+
+        funPointer fptr2 = &FirstNInstance;
+        for(std::shared_ptr<TouristVehicle> value : Operation(touristObjects, fptr2))
         {
             std::cout << *value << "\n";
         }
