ba454665b2c15c2690ded1293513b06b2e36cf6f
ShubhamAwale <shubhamawale651@gmail.com>
2023-09-08 14:07:40 +0530

    implementation of template and understand it

---
+++ Modern_C/Week3/8Sept23/template2.cpp
@@ -0,0 +1,48 @@
+/*
+    Display() :
+        It should accept a container of vector
+        of primitive data type or pointers
+        to primitive data type and print all the items
+        one by one
+*/
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void Display(std::vector<T> data) // vector of values template
+{
+    for (T &value : data)
+    {
+        std::cout << value << '\t';
+    }
+    std::cout << '\n';
+}
+
+template <typename T>
+void Display(std::vector<T *> data) // vector of pointer template
+{
+    for (T *&value : data)
+    {
+        std::cout << *value << '\t';
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+
+    std::vector v1{1, 2, 4, 5, 6};
+    int n1 = 10;
+
+    int *p1 = &n1;
+    int *p2 = &n1;
+    int *p3 = &n1;
+    int *p4 = &n1;
+
+    std::vector v2{p1,p2,p3,p4}; // store address
+
+    Display<int>(v1);
+    Display<int*>(v2);
+    
+}
